18bf388a6bdb35b92e9fb5613a3ff677117cf830
Lisa Folliard & Janice Dunn <student@epicodus.com>
2015-09-09 14:26:05 -0700

    move the first two consonants to the end and add 'ay'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
 var pigLatin = function(word) {
-  //vowels = ["a", "e", "i", "o", "u"]
+  var vowels = ["a", "e", "i", "o", "u"];
 
   var first_letter = word.charAt(0);
+  var second_letter = word.charAt(1);
 
-  if (first_letter === ("a" || "e" || "i" || "o" || "u")) {
-    word = word.concat("ay");
+  if (vowels.indexOf(first_letter) === 0) {
+    var word = word.concat("ay");
     return word;
-  } else if (first_letter !== ("a" || "e" || "i" || "o" || "u")) {
+
+  } else if ((vowels.indexOf(first_letter) === -1) && (vowels.indexOf(second_letter) === -1)) {
+    var two_letters = word.slice(0, 2);
+    var new_word = word.substr(2).concat(two_letters).concat("ay");
+    return new_word;
+
+  } else if (vowels.indexOf(first_letter) === -1) {
     var letter = word.slice(0, 1);
     var new_word = word.substr(1).concat(letter).concat("ay");
     return new_word;
   }
-
 };
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -3,6 +3,10 @@ describe('pigLatin', function() {
     expect(pigLatin("apple")).to.equal("appleay");
   });
 
+  it('will move the first two consonants to the end and add "ay"', function() {
+    expect(pigLatin("crater")).to.equal("atercray");
+  });
+
   it('will move the consonant to the end and add "ay"', function() {
     expect(pigLatin("peach")).to.equal("eachpay");
   });
